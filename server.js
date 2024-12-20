338f2d25a3c9fa86d582911fdd0edd020e548330
josiahpogi <josiahawa.ao@gmail.com>
2024-12-19 17:46:26 -0800

    chat fix

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const { connectToChat, sendMessage, sendFile, userDisconnect } = require('./cont
 
 dotenv.config();
 const app = express();
-
-// Create an HTTP server and attach socket.io
 const server = http.createServer(app);
 const io = socketIo(server);
 
@@ -29,25 +27,22 @@ app.use('/sms', smsRoutes);
 io.on('connection', (socket) => {
   console.log(`User connected: ${socket.id}`);
 
-  // Start a chat session when a user connects
+  // Start a chat session for the user
   connectToChat(socket, io);
 
   // Handle incoming chat messages
-  socket.on('send-message', (message) => {
-    console.log('Received message: ', message);
-    sendMessage(socket, message); // sendMessage should broadcast this message to the other user.
+  socket.on('chat-message', (message) => {
+    sendMessage(socket, message);
   });
 
   // Handle incoming file attachments
   socket.on('send-file', (fileData) => {
-    console.log('Received file data');
-    sendFile(socket, fileData); // sendFile should broadcast the file to the other user.
+    sendFile(socket, fileData);
   });
 
   // Handle user disconnection
   socket.on('disconnect', () => {
-    console.log(`User disconnected: ${socket.id}`);
-    userDisconnect(socket, io); // This will remove the user from the waiting list and notify the other user
+    userDisconnect(socket, io);
   });
 });
 
